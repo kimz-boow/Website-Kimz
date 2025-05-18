50f96e4cb3c62fe840998f4fdfdfc2700f73da49
kimz-boow <hakimputrap07@gmail.com>
2025-05-18 14:50:23 +0700

    Add files via upload

---
+++ cake.js
@@ -0,0 +1,6 @@
+// Tambahan efek sederhana: munculkan alert setelah animasi selesai
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    alert('Make a wish, Lovie!');
+  }, 8000); // setelah animasi kue selesai
+});
